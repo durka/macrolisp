0a54e2e4473850dc6f867a56340f0b186dcb2ad5
Alex Burka <aburka@seas.upenn.edu>
2015-06-27 17:11:56 -0400

    all-lisp iterative factorial

--- tests/test.rs
+++ tests/test.rs
@@ -12,16 +12,12 @@ fn main() {
                                        (_mul: a (_recur: (_sub: a 1)))))
     );*/
     let factorial = lisp!(
-        (lambda ((a i32) -> i32) (_rust:
-                                        {
-                                            let mut x = a;
-                                            let mut acc = 1;
-                                            while x > 1 {
-                                                acc *= x;
-                                                x -= 1;
-                                            }
-                                            acc
-                                        }))
+        (lambda ((a i32) -> i32) (_let: mut x a)
+                                 (_let: mut acc 1)
+                                 (_while: (_gt: x 1)
+                                  (_set: acc (_mul: acc x))
+                                  (_set: x   (_sub: x 1)))
+                                 acc)
     );
     
     println!("1+2+3+4 = {}", lisp!( (add: 1 (add: 2 (add: 3 4))) ));
