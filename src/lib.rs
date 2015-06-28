ce437e7c586f2f483abd1a37eec94f6de96aca44
Alex Burka <aburka@seas.upenn.edu>
2015-06-27 23:40:31 -0400

    recursive lambda special form

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,34 @@
-#![feature(trace_macros)]
-//trace_macros!(true);
-
 pub mod prelude;
-                  
+
 #[macro_export] macro_rules! lisp {
     // special forms
     ((lambda ($(($argn:ident $argt:ty))* -> $ret:ty) $($body:tt)*)) => {
+        // regular lambda
         |$($argn:$argt),*| -> $ret { $(lisp!($body));* }
     };
+    ((lambda $s:ident ($(($argn:ident $argt:ty))* -> $ret:ty) $($body:tt)*)) => {{
+        // recursive lambda
+        // $s MUST be "self"
+        // recurse by calling (self ...)
+        struct F;
+        impl FnOnce<($($argt,)*)> for F {
+            type Output = $ret;
+            extern "rust-call" fn call_once($s, ($($argn,)*): ($($argt,)*)) -> $ret {
+                $(lisp!($body));*
+            }
+        }
+        impl FnMut<($($argt,)*)> for F {
+            extern "rust-call" fn call_mut(&mut $s, ($($argn,)*): ($($argt,)*)) -> $ret {
+                $(lisp!($body));*
+            }
+        }
+        impl Fn<($($argt,)*)> for F {
+            extern "rust-call" fn call(&$s, ($($argn,)*): ($($argt,)*)) -> $ret {
+                $(lisp!($body));*
+            }
+        }
+        F
+    }};
     ((defn $name:ident ($(($argn:ident $argt:ty))* -> $ret:ty) $($body:tt)*)) => {
         fn $name($($argn:$argt),*) -> $ret { $(lisp!($body));* }
     };
@@ -17,6 +38,9 @@ pub mod prelude;
     ((_while $cond:tt $($body:tt)*)) => {
         while lisp!($cond) { $(lisp!($body));* }
     };
+    /*((_recur $($arg:tt)*)) => {
+        self.call($(lisp!($arg)),*)
+    };*/
 
     // variables
     ((_let mut $var:ident $val:tt)) => {
--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,6 @@
+#![feature(core, unboxed_closures, trace_macros)]
+//trace_macros!(true);
+
 #[macro_use] extern crate macrolisp;
 use macrolisp::prelude::*;
 
@@ -6,7 +9,7 @@ fn main() {
     let add = lisp!(
         (lambda ((a i32) (b i32) -> i32) (_add a b))
     );
-    let factorial = lisp!(
+    let factorial_proc = lisp!(
         (lambda ((a i32) -> i32) (_let mut x a)
                                  (_let mut acc 1)
                                  (_while (_gt x 1)
@@ -15,13 +18,19 @@ fn main() {
                                  acc)
     );
     lisp!(
-        (defn factorial_rec ((a i32) -> i32) (_if (_eq a 1)
-                                                  1
-                                                  (_mul a (factorial_rec (_sub a 1)))))
+        (defn factorial ((a i32) -> i32) (_if (_eq a 1)
+                                              1
+                                              (_mul a (factorial (_sub a 1)))))
+    );
+    let factorial_rec = lisp!(
+        (lambda self ((a i32) -> i32) (_if (_eq a 1)
+                                           1
+                                           (_mul a (self (_sub a 1)))))
     );
     
     println!("1+2+3+4 = {}", lisp!( (add 1 (add 2 (add 3 4))) ));
-    println!("5! = {}", lisp!( (factorial 5) ));
-    println!("10! = {}", lisp!( (factorial_rec 10) ));
+    println!("5! = {}", lisp!( (factorial_proc 5) ));
+    println!("6! = {}", lisp!( (factorial 6) ));
+    println!("7! = {}", lisp!( (factorial_rec 7) ));
 }
 
