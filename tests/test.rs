962e1e8814b5fe7ee77ec5e701f00a1c39c010e5
Alex Burka <aburka@seas.upenn.edu>
2015-06-28 10:51:52 -0400

    formatting

--- tests/test.rs
+++ tests/test.rs
@@ -7,25 +7,34 @@ use macrolisp::prelude::*;
 #[test]
 fn main() {
     let add = lisp!(
-        (lambda (((a i32) (b i32)) i32) (_add a b))
+        (lambda (((a i32)
+                  (b i32))
+                 i32)
+         (_add a b))
     );
     let factorial_proc = lisp!(
-        (lambda (((a i32)) i32) (_let mut x a)
-                                 (_let mut acc 1)
-                                 (_while (_gt x 1)
-                                  (_set acc (_mul acc x))
-                                  (_set x   (_sub x 1)))
-                                 acc)
+        (lambda (((a i32))
+                 i32)
+         (_let mut x a)
+         (_let mut acc 1)
+         (_while (_gt x 1)
+          (_set acc (_mul acc x))
+          (_set x   (_sub x 1)))
+         acc)
     );
     lisp!(
-        (defn factorial (((a i32)) i32) (_if (_eq a 1)
-                                              1
-                                              (_mul a (factorial (_sub a 1)))))
+        (defn factorial (((a i32))
+                         i32)
+         (_if (_eq a 1)
+              1
+              (_mul a (factorial (_sub a 1)))))
     );
     let factorial_rec = lisp!(
-        (lambda self (((a i32)) i32) (_if (_eq a 1)
-                                           1
-                                           (_mul a (self (_sub a 1)))))
+        (lambda self (((a i32))
+                      i32)
+         (_if (_eq a 1)
+              1
+              (_mul a (self (_sub a 1)))))
     );
     
     println!("1+2+3+4 = {}", lisp!( (add 1 (add 2 (add 3 4))) ));
